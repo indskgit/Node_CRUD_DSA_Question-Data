60721c1076497e8e196f997260b7bc84b4670c94
Saurabh Kumar <skumarsaurabh2001@gmail.com>
2023-11-22 01:25:48 +0530

    updated UI

--- crud-nodejs-mysql-master/src/index.js
+++ crud-nodejs-mysql-master/src/index.js
@@ -1,5 +1,6 @@
+import { request } from "express";
 import app from "./app.js";
-import { port } from "./config.js";
+import { port } from "./config.js";``
 
 app.listen(port);
 console.log("server running on "  + "https//localhost:" +port);
