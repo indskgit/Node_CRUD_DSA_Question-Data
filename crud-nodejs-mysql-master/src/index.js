1ff93fc891f6e39c0ac1f6db6e8fcabf77877949
Saurabh Kumar <skumarsaurabh2001@gmail.com>
2023-11-26 19:29:20 +0530

    UI update

--- crud-nodejs-mysql-master/src/index.js
+++ crud-nodejs-mysql-master/src/index.js
@@ -2,5 +2,6 @@ import { request } from "express";
 import app from "./app.js";
 import { port } from "./config.js";``
 
+
 app.listen(port);
-console.log("server running on "  + "https//localhost:" +port);
+console.log("start http://localhost:3000");
